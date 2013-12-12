4264a3a8d9e4cbfb8373710f8e5b824b842c4bae
bruppi <bruppi@camelotsmm.com>
2013-12-12 15:10:28 -0600

    3

--- js/dma_assets.js
+++ js/dma_assets.js
@@ -1,3 +1,8 @@
-$(document.ready)function(){
-	$("#PromotionalSection").hide();
-};
+//On Page Load
+$(document).ready(function(){
+	//Check for params
+	if(source.toLowerCase().indexOf("?src=test")>=0){
+		//If params set, then hide page
+		$('#PromotionalSection').hide();
+	}
+});
